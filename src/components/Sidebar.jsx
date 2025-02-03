775dd04118154465e0dec9ec7be581a83613f6c9
Alina <ms.sciurus@gmail.com>
2025-02-03 15:26:34 +0200

    nested routes and index route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import AppNav from './AppNav';
 import Footer from './Footer';
 import Logo from './Logo';
@@ -5,10 +6,12 @@ import styles from './Sidebar.module.css';
 
 function Sidebar() {
   return (
-    <div className={styles.sidebsr}>
+    <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+
+      <Outlet />
+
       <Footer />
     </div>
   );
